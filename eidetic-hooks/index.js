58326729c38a51566b9ae785683216582a6b697a
SmileRush <dbghrgksms7@gmail.com>
2021-10-07 21:26:11 +0900

    Update index.js

--- eidetic-hooks/index.js
+++ eidetic-hooks/index.js
@@ -1,1 +1,17 @@
-export { useInput } from "./useInput";
+import { useState } from "react";
+
+export const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = event => {
+    const { target: { value } } = event;
+    let willUpdate = true
+    if (typeof validator === "function") {
+      willUpdate = validator(value)
+    }
+    if (willUpdate) {
+      setValue(value)
+    }
+  }
+  return { value, onChange };
+};
+// export { useInput } from "./useInput";

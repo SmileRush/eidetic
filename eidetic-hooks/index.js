a123b6b3a2ad51f15fec2ddd3c60264aeb5757ce
SmileRush <dbghrgksms7@gmail.com>
2021-10-08 14:52:29 +0900

    {} => [] commit

--- eidetic-hooks/index.js
+++ eidetic-hooks/index.js
@@ -1,16 +1,1 @@
-import { useState } from "react";
-
-export const useInput = (initialValue, validator) => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = event => {
-    const { target: { value } } = event;
-    let willUpdate = true
-    if (typeof validator === "function") {
-      willUpdate = validator(value)
-    }
-    if (willUpdate) {
-      setValue(value)
-    }
-  }
-  return { value, onChange };
-};
+export { useInput } from './useInput'
